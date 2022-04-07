8ae92e3c9798eee5bcc11aa8f5d62e01b0671464
Thanatos <polaroids.of.tarkovsky@gmail.com>
2022-04-07 19:48:31 +0300

    Homework-4

---
+++ lesson-04-spring-boot/src/main/java/ru/geekbrains/persist/ProductRepository.java
@@ -0,0 +1,48 @@
+package ru.geekbrains.persist;
+
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class ProductRepository {
+
+    private final Map<Long, Product> productMap = new ConcurrentHashMap<>();
+
+    private final AtomicLong identity = new AtomicLong(0);
+
+    @PostConstruct
+    public void init() {
+        this.save(new Product( "Product 1", 100000.0));
+        this.save(new Product( "Product 2", 200000.0));
+        this.save(new Product( "Product 3", 300000.0));
+        this.save(new Product( "Product 4", 400000.0));
+        this.save(new Product( "Product 5", 500000.0));
+    }
+
+    public List<Product> findAll() {
+        return new ArrayList<>(productMap.values());
+    }
+
+    public Product findById(long id) {
+        return productMap.get(id);
+    }
+
+    public Product save(Product product) {
+        if (product.getId() == null) {
+            product.setId(identity.incrementAndGet());
+        }
+        productMap.put(product.getId(), product);
+        return product;
+    }
+
+    public void delete(long id) {
+        productMap.remove(id);
+    }
+
+}
