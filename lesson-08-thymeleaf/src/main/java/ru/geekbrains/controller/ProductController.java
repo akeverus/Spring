d19cc987f8cf61664d720ed718a6eee062357bb9
Thanatos <polaroids.of.tarkovsky@gmail.com>
2022-04-20 18:35:21 +0300

    homework_8

--- lesson-08-thymeleaf/src/main/java/ru/geekbrains/controller/ProductController.java
+++ lesson-08-thymeleaf/src/main/java/ru/geekbrains/controller/ProductController.java
@@ -1,39 +1,99 @@
 package ru.geekbrains.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.geekbrains.model.Product;
 import ru.geekbrains.service.ProductService;
 
 import java.util.List;
-import java.util.Optional;
 
-@RestController
-@RequestMapping("/product")
+@Controller
 public class ProductController {
 
-    private ProductService productService;
+    private final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
 
-    @PostMapping
-    public Product create(@RequestBody Product product) {
-        return productService.create(product);
+    @GetMapping("/index")
+    public String showProductList(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "index";
     }
 
-    @GetMapping("/{id}")
-    public Optional<Product> findById(@PathVariable Long id) {
-        return productService.findById(id);
+    @GetMapping("/signup")
+    public String showSignUpForm(Product product) {
+        return "add-product";
     }
 
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable Long id) {
+    @PostMapping("/addproduct")
+    public String addProduct(@Valid Product product, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "add-product";
+        }
+
+        productService.create(product);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String showUpdateForm(@PathVariable("id") long id, Model model) {
+        Product product = productService.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid product Id:" + id));
+        model.addAttribute("product", product);
+
+        return "update-product";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updateProduct(@PathVariable("id") long id, @Valid Product product, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            product.setId(id);
+            return "update-product";
+        }
+
+        productService.create(product);
+
+        return "redirect:/index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteProduct(@PathVariable("id") long id, Model model) {
         productService.delete(id);
+        return "redirect:/index";
     }
 
-    @GetMapping("/list")
-    public List<Product> findAll() {
-        return productService.findAll();
+    @GetMapping("/page/{pageNo}")
+    public String findPaginated(@PathVariable (value = "pageNo") int pageNo,
+                                @RequestParam("sortField") String sortField,
+                                @RequestParam("sortDir") String sortDir,
+                                Model model) {
+
+        int pageSize = 5;
+
+        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortField).ascending() :
+                Sort.by(sortField).descending();
+        Pageable pageable = PageRequest.of(pageNo, pageSize, sort);
+
+        Page<Product> page = productService.findPaginated(pageable);
+        List<Product> listProducts = page.getContent();
+
+        model.addAttribute("currentPage", pageNo);
+        model.addAttribute("totalPages", page.getTotalPages());
+        model.addAttribute("totalItems", page.getTotalElements());
+
+        model.addAttribute("sortField", sortField);
+        model.addAttribute("sortDir", sortDir);
+        model.addAttribute("reverseSortDir", sortDir.equals("asc") ? "desc" : "asc");
+
+        model.addAttribute("listProducts", listProducts);
+        return "index";
     }
 }
